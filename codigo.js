8c535aa4b2d9b82a09a08c94afd674734a5f4050
Marcelo Mansilla <mansilladmarcelo@gmail.com>
2024-06-11 17:01:26 -0300

    add back on top js

---
+++ codigo.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    window.onscroll = function() {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("backToTop").style.display = "block";
+        } else {
+            document.getElementById("backToTop").style.display = "none";
+        }
+    }
+
+    document.getElementById('backToTop').addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
